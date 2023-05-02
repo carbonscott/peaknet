f65d5128da3906cd499fd8a5f2975f85c0d38290
carbonscott <wangimagine@gmail.com>
2023-05-01 23:31:23 -0700

    Init commit.

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="min-pf",
+    version="23.05.01",
+    author="Cong Wang",
+    author_email="wangimagine@gmail.com",
+    description="A neural network model finding Bragg peaks in SFX experiments.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/carbonscott/peaknet",
+    keywords = ['SFX', 'X-ray', 'Neural network model'],
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
