dba1e3c7a152c9cfc4955b902767050e4b21af5b
carbonscott <wangimagine@gmail.com>
2023-05-02 17:32:50 -0700

    Support model loading without module.  Also add a utility function to remove module from a chkpt.

--- min_pf/utils.py
+++ min_pf/utils.py
@@ -356,7 +356,7 @@ def save_checkpoint(model, optimizer, scheduler, epoch, loss_min, path):
     torch.save({
         'epoch'               : epoch,
         'loss_min'            : loss_min,
-        'model_state_dict'    : model.state_dict(),
+        'model_state_dict'    : model.module.state_dict() if hasattr(model, 'module') else model.state_dict(),
         'optimizer_state_dict': optimizer.state_dict(),
         'scheduler_state_dict': scheduler.state_dict(),
     }, path)
@@ -366,8 +366,20 @@ def save_checkpoint(model, optimizer, scheduler, epoch, loss_min, path):
 
 def load_checkpoint(model, optimizer, scheduler, path):
     checkpoint = torch.load(path)
-    model.load_state_dict(checkpoint['model_state_dict'])
-    optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
-    scheduler.load_state_dict(checkpoint['scheduler_state_dict'])
+    if model     is not None: model.module.load_state_dict(checkpoint['model_state_dict']) \
+                              if hasattr(model, 'module') else \
+                              model.load_state_dict(checkpoint['model_state_dict'])
+    if optimizer is not None: optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+    if scheduler is not None: scheduler.load_state_dict(checkpoint['scheduler_state_dict'])
 
     return checkpoint['epoch'], checkpoint['loss_min']
+
+
+
+
+def remove_module_from_state_dict(state_dict):
+    new_state_dict = {}
+    for key, value in state_dict.items():
+        new_key = key.replace("module.", "")  # Remove the "module." prefix
+        new_state_dict[new_key] = value
+    return new_state_dict
