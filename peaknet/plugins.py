1bcc4debd8def4d7db2c056125df3efd08d5fd73
carbonscott <wangimagine@gmail.com>
2023-05-18 09:23:57 -0700

    Add masking to plugins.

--- peaknet/plugins.py
+++ peaknet/plugins.py
@@ -696,3 +696,26 @@ def remove_outliers(data, percentile = 5):
     upper_bound = q3 + 1.5 * iqr
     mask = np.logical_and(data >= lower_bound, data <= upper_bound)
     return data * mask
+
+
+
+
+def apply_mask(data, mask, mask_value = np.nan):
+    """ 
+    Return masked data.
+
+    Args:
+        data: numpy.ndarray with the shape of (B, H, W).·
+              - B: batch of images.
+              - H: height of an image.
+              - W: width of an image.
+
+        mask: numpy.ndarray with the shape of (B, H, W).·
+
+    Returns:
+        data_masked: numpy.ndarray.
+    """ 
+    # Mask unwanted pixels with np.nan...
+    data_masked = np.where(mask, data, mask_value)
+
+    return data_masked
