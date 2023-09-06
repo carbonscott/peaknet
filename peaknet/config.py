bca6d3524884904ec2e63e60392b28c662e67de1
carbonscott <wangimagine@gmail.com>
2023-09-06 12:55:00 -0700

    Bookkeeping.

--- peaknet/config.py
+++ peaknet/config.py
@@ -2,5 +2,11 @@ from .utils import Configurator
 
 CONFIG = Configurator()
 with CONFIG.enable_auto_create():
-    CONFIG.RESSTEM.BN.EPS      = 1e-5
-    CONFIG.RESSTEM.BN.MOMENTUM = 1e-1
+    CONFIG.RESSTEM.BN.EPS       = 1e-5
+    CONFIG.RESSTEM.BN.MOMENTUM  = 1e-1
+    CONFIG.RESSTEM.RELU_INPLACE = True
+
+with CONFIG.enable_auto_create():
+    CONFIG.RESBLOCK.BN.EPS       = 1e-5
+    CONFIG.RESBLOCK.BN.MOMENTUM  = 1e-1
+    CONFIG.RESBLOCK.RELU_INPLACE = True
--- peaknet/modeling/regnet.py
+++ peaknet/modeling/regnet.py
@@ -20,7 +20,7 @@ import torch.nn.functional as F
 
 from ..config import CONFIG
 
-from blocks import conv2d, pool2d
+from .blocks import conv2d, pool2d
 
 
 class ResStem(nn.Module):
@@ -36,12 +36,12 @@ class ResStem(nn.Module):
     """
 
     def __init__(self, in_channels, out_channels):
-        super(ResStem, self).__init__()
+        super().__init__()
 
         print(CONFIG.RESSTEM.BN.EPS)
         self.conv = conv2d(in_channels,
                            out_channels,
-                           kernel_size = (7, 7),
+                           kernel_size = 7,
                            stride      = 2,)
         self.bn   = nn.BatchNorm2d(num_features = out_channels,
                                    eps          = CONFIG.RESSTEM.BN.EPS,
@@ -56,3 +56,36 @@ class ResStem(nn.Module):
             x = layer(x)
 
         return x
+
+
+
+
+class ResStage(nn.Module):
+    """
+    This class implments the four stages in the RegNet architecture.
+    """
+
+    def __init__(self):
+        pass
+
+
+
+
+class ResBlock(nn.Module):
+    """
+    Create X blocks for the RegNet architecture.
+    """
+
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+
+        self.conv1x1 = nn.Sequential(
+            conv2d(in_channels,
+                   out_channels,
+                   kernel_size = 1,
+                   stride      = 1,),
+            nn.BatchNorm2d(num_features = out_channels,
+                           eps          = CONFIG.RESBLOCK.BN.EPS,
+                           momentum     = CONFIG.RESBLOCK.BN.MOMENTUM,),
+            nn.ReLU(inplace = CONFIG.RESBLOCK.RELU_INPLACE),
+        )
