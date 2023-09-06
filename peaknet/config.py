bdc0e3422e170360894365b87477303f38233d51
carbonscott <wangimagine@gmail.com>
2023-09-05 22:54:06 -0700

    I am much happier with the configurator that allows auto-create attributes in a context manager.

--- peaknet/config.py
+++ peaknet/config.py
@@ -1,4 +1,6 @@
-from .utils import DictContainer
+from .utils import Configurator
 
-CONFIG = DictContainer()
-CONFIG.RESSTEM.BN.EPS = 1e-5
+CONFIG = Configurator()
+with CONFIG.enable_auto_create():
+    CONFIG.RESSTEM.BN.EPS = 1e-5
+    CONFIG.RESSTEM.BN.MOMENTUM = 1e-1
