93045ee35349c474ec475b587245228643939e69
carbonscott <wangimagine@gmail.com>
2023-09-06 22:29:39 -0700

    Use a better naming to distinguish conv, block and stage.

--- peaknet/modeling/regnet.py
+++ peaknet/modeling/regnet.py
@@ -35,15 +35,15 @@ class ResStem(nn.Module):
     - MaxPool
     """
 
-    def __init__(self, in_channels, out_channels):
+    def __init__(self, stem_in_channels, stem_out_channels):
         super().__init__()
 
         print(CONFIG.RESSTEM.BN.EPS)
-        self.conv = conv2d(in_channels,
-                           out_channels,
+        self.conv = conv2d(stem_in_channels,
+                           stem_out_channels,
                            kernel_size = 7,
                            stride      = 2,)
-        self.bn   = nn.BatchNorm2d(num_features = out_channels,
+        self.bn   = nn.BatchNorm2d(num_features = stem_out_channels,
                                    eps          = CONFIG.RESSTEM.BN.EPS,
                                    momentum     = CONFIG.RESSTEM.BN.MOMENTUM,)
 
@@ -67,50 +67,50 @@ class ResBlock(nn.Module):
     in_channels, mid_channels (bottleneck channels), out_channels
     """
 
-    def __init__(self, in_channels, out_channels, mid_channels, mid_groups = 1, in_stride = 1):
+    def __init__(self, block_in_channels, block_out_channels, mid_conv_channels, mid_conv_groups = 1, in_conv_stride = 1):
         super().__init__()
 
         self.in_conv = nn.Sequential(
-            conv2d(in_channels,
-                   mid_channels,
+            conv2d(block_in_channels,
+                   mid_conv_channels,
                    kernel_size = 1,
-                   stride      = in_stride,),
-            nn.BatchNorm2d(num_features = mid_channels,
+                   stride      = in_conv_stride,),
+            nn.BatchNorm2d(num_features = mid_conv_channels,
                            eps          = CONFIG.RESBLOCK.BN.EPS,
                            momentum     = CONFIG.RESBLOCK.BN.MOMENTUM,),
             nn.ReLU(inplace = CONFIG.RESBLOCK.RELU_INPLACE),
         )
 
         self.mid_conv = nn.Sequential(
-            conv2d(mid_channels,
-                   mid_channels,
+            conv2d(mid_conv_channels,
+                   mid_conv_channels,
                    kernel_size = 3,
                    stride      = 1,
-                   groups      = mid_groups),
-            nn.BatchNorm2d(num_features = mid_channels,
+                   groups      = mid_conv_groups),
+            nn.BatchNorm2d(num_features = mid_conv_channels,
                            eps          = CONFIG.RESBLOCK.BN.EPS,
                            momentum     = CONFIG.RESBLOCK.BN.MOMENTUM,),
             nn.ReLU(inplace = CONFIG.RESBLOCK.RELU_INPLACE),
         )
 
         self.out_conv = nn.Sequential(
-            conv2d(mid_channels,
-                   out_channels,
+            conv2d(mid_conv_channels,
+                   block_out_channels,
                    kernel_size = 1,
                    stride      = 1,),
-            nn.BatchNorm2d(num_features = out_channels,
+            nn.BatchNorm2d(num_features = block_out_channels,
                            eps          = CONFIG.RESBLOCK.BN.EPS,
                            momentum     = CONFIG.RESBLOCK.BN.MOMENTUM,),
         )
 
         self.res_conv = None
-        if (in_channels != out_channels) or (in_stride != 1):
+        if (block_in_channels != block_out_channels) or (in_conv_stride != 1):
             self.res_conv = nn.Sequential(
-                conv2d(in_channels,
-                       out_channels,
+                conv2d(block_in_channels,
+                       block_out_channels,
                        kernel_size = 1,
-                       stride      = in_stride,),
-                nn.BatchNorm2d(num_features = out_channels,
+                       stride      = in_conv_stride,),
+                nn.BatchNorm2d(num_features = block_out_channels,
                                eps          = CONFIG.RESBLOCK.BN.EPS,
                                momentum     = CONFIG.RESBLOCK.BN.MOMENTUM,),
             )
@@ -140,15 +140,21 @@ class ResStage(nn.Module):
     Block means a res block.
     """
 
-    def __init__(self, in_channels, out_channels, num_blocks, mid_channels, mid_groups, in_stride):
+    def __init__(self, stage_in_channels, stage_out_channels, num_blocks, mid_conv_channels, mid_conv_groups, in_conv_stride):
         super().__init__()
 
         self.blocks = nn.ModuleList([
-            ResBlock(in_channels  = in_channels if block_idx == 0 else out_channels, # First block uses in_channel and rest uses prev out_channels
-                     out_channels = out_channels,
-                     mid_channels = mid_channels,
-                     in_stride    = in_stride  if block_idx == 0 else 1, # First block uses in_stride and rest uses 1
-                     mid_groups   = mid_groups)
+            ResBlock(
+                # First block uses stage_in_channel and rest uses prev stage_out_channels...
+                block_in_channels  = stage_in_channels if block_idx == 0 else stage_out_channels,
+
+                block_out_channels = stage_out_channels,
+                mid_conv_channels  = mid_conv_channels,
+                mid_conv_groups    = mid_conv_groups,
+
+                # First block uses in_conv_stride and rest uses 1...
+                in_conv_stride     = in_conv_stride  if block_idx == 0 else 1,
+            )
             for block_idx in range(num_blocks)
         ])
 
@@ -173,11 +179,11 @@ class ResNet50(nn.Module):
     def __init__(self):
         super().__init__()
 
-        self.stem = ResStem(in_channels = 1, out_channels = 64)
+        self.stem = ResStem(stem_in_channels = 1, stem_out_channels = 64)
 
-        self.stage1 = ResStage(in_channels  = 64,
-                               out_channels = 256,
-                               num_blocks   = 3,
-                               mid_channels = 64,
-                               mid_groups   = 1,
-                               in_stride    = 1,)
+        self.stage1 = ResStage(stage_in_channels  = 64,
+                               stage_out_channels = 256,
+                               num_blocks         = 3,
+                               mid_conv_channels  = 64,
+                               mid_conv_groups    = 1,
+                               in_conv_stride     = 1,)
