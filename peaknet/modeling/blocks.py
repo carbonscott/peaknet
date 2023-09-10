aad5908712499d86585bc16c513b8201c166679d
carbonscott <wangimagine@gmail.com>
2023-09-10 14:27:24 -0700

    Make the padding term explicit.

--- peaknet/modeling/blocks.py
+++ peaknet/modeling/blocks.py
@@ -12,7 +12,9 @@ from ..config import CONFIG
 def conv2d(in_channels, out_channels, kernel_size, *, stride = 1, groups = 1, bias = False):    # ...`*` forces the rest arguments to be keyword arguments
     """Helper for building a conv2d layer."""
     assert kernel_size % 2 == 1, "Only odd size kernels supported to avoid padding issues."
+
     padding = (kernel_size - 1)//2
+
     return nn.Conv2d(in_channels,
                      out_channels,
                      kernel_size = kernel_size,
@@ -25,4 +27,7 @@ def conv2d(in_channels, out_channels, kernel_size, *, stride = 1, groups = 1, bi
 def pool2d(kernel_size, *, stride = 2):    # ...`*` forces the rest arguments to be keyword arguments
     """Helper for building a pool2d layer."""
     assert kernel_size % 2 == 1, "Only odd size kernels supported to avoid padding issues."
-    return nn.MaxPool2d(kernel_size = kernel_size, stride = stride, padding = (kernel_size - 1)//2)
+
+    padding = (kernel_size - 1)//2
+
+    return nn.MaxPool2d(kernel_size = kernel_size, stride = stride, padding = padding)
