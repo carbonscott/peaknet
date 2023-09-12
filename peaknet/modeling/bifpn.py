5bafb6b797ade98137ae50a3e473ea80c1cf0881
carbonscott <wangimagine@gmail.com>
2023-09-11 21:57:21 -0700

    Bookkeep the flexible implementation of BiFPN.  Also indicate the previous version is for learning and debugging purposes only.

--- peaknet/modeling/bifpn.py
+++ peaknet/modeling/bifpn.py
@@ -67,6 +67,137 @@ class DepthwiseSeparableConv2d(nn.Module):
 class BiFPNBlock(nn.Module):
     """
     One BiFPN block takes feature maps at five different scales:
+    (p3, p4, ..., pN).
+
+    Notice that the input to BiFPNBlock should have the same channel size, 
+    which can be achieved with a conv layer in the upstream.
+    """
+
+    def __init__(self, num_features = 64, num_levels = 5):
+        super().__init__()
+
+        # Follow the paper's notation with base level starting from "3"...
+        BASE_LEVEL = 3
+
+        # Confusingly, there should be at least 3 levels in total...
+        num_levels = max(num_levels, 3)
+
+        # Decide the min max level...
+        min_level  = BASE_LEVEL
+        max_level  = BASE_LEVEL + (num_levels - 1)
+
+        # Create conv2d layers for fusion stage M...
+        # min_level, ..., max_level - 1
+        m_conv = nn.ModuleDict({
+            f"m{level}" : nn.Sequential(
+                DepthwiseSeparableConv2d(in_channels  = num_features,
+                                         out_channels = num_features,
+                                         bias         = False),
+                nn.BatchNorm2d(num_features = num_features,
+                               eps          = CONFIG.BIFPN.BN.EPS,
+                               momentum     = CONFIG.BIFPN.BN.MOMENTUM),
+                nn.ReLU(),
+            )
+            for level in range(min_level, max_level)
+        })
+
+        # Create conv2d layers for fusion stage Q...
+        # min_level + 1, max_level
+        q_conv = nn.ModuleDict({
+            f"q{level}" : nn.Sequential(
+                DepthwiseSeparableConv2d(in_channels  = num_features,
+                                         out_channels = num_features,
+                                         bias         = False),
+                nn.BatchNorm2d(num_features = num_features,
+                               eps          = CONFIG.BIFPN.BN.EPS,
+                               momentum     = CONFIG.BIFPN.BN.MOMENTUM),
+                nn.ReLU(),
+            )
+            for level in range(min_level + 1, max_level + 1)
+        })
+
+        self.conv = nn.ModuleDict()
+        self.conv.update(m_conv)
+        self.conv.update(q_conv)
+
+        # Define the weights used in fusion
+        num_level_stage_m = max_level - 1 - min_level
+        num_level_stage_q = num_fusion_layer_in_stage_m
+        self.w_m = nn.Parameter(torch.randn(num_level_stage_m, 2))    # Two-component fusion at stage M
+        self.w_q = nn.Parameter(torch.randn(num_level_stage_q, 3))    # Three-component fusion at stage Q
+
+        # Keep these numbers as attributes...
+        self.BASE_LEVEL = BASE_LEVEL
+        self.min_level  = min_level
+        self.max_level  = max_level
+
+
+    def forward(self, x):
+        # Keep these numbers as attributes...
+        BASE_LEVEL = self.BASE_LEVEL
+        min_level  = self.min_level
+        max_level  = self.max_level
+        num_levels = max_level - min_level + 1
+
+        # Unpack feature maps into dict...
+        # x is 0-based index
+        # (B, C, [H], [W])
+        p = { level : x[idx] for idx, level in enumerate(range(min_level, min_level + num_levels)) }
+
+        # ___/ Stage M \___
+        m = {}
+        for idx, level_low in enumerate(range(max_level, min_level, -1)):
+            level_high = level_low - 1
+            m_low   = p[level_low ] if idx == 0 else m[level_low]
+            p_high  = p[level_high]
+
+            w1, w2 = self.w_m[idx]
+            m_low_up = F.interpolate(m_low,
+                                     scale_factor  = CONFIG.BIFPN.UP_SCALE_FACTOR,
+                                     mode          = 'bilinear',
+                                     align_corners = False)
+            m_fused  = w1 * p_high + w2 * m_low_up
+            m_fused /= (w1 + w2 + CONFIG.BIFPN.FUSION.EPS)
+            m_fused  = self.conv[f"m{level_high}"](m_fused)
+
+            m[level_high] = m_fused
+
+        # ___/ Stage Q \___
+        q = {}
+        for idx, level_high in enumerate(range(min_level, max_level)):
+            level_low = level_high + 1
+            q_high = m[level_high] if idx == 0             else q[level_high]
+            m_low  = m[level_low]  if idx < num_levels - 1 else q[level_low ]
+            p_low  = p[level_low]
+
+            w1, w2, w3 = self.w_q[idx]
+            q_high_up = F.interpolate(q_high,
+                                      scale_factor  = CONFIG.BIFPN.DOWN_SCALE_FACTOR,
+                                      mode          = 'bilinear',
+                                      align_corners = False)
+            q_fused  = w1 * p_low + w2 * m_low + w3 * q_high_up
+            q_fused /= (w1 + w2 + w3 + CONFIG.BIFPN.FUSION.EPS)
+            q_fused  = self.conv[f"q{level_low}"](q_fused)
+
+            q[level_low] = q_fused
+
+        return [ q[level] for level in range(min_level, max_level) ]
+
+
+
+
+
+#####################################################################
+# RAW BIFPN IMPLEMENTATION BELOW IS FOR LEARNING/DEBUGGING PURPOSES #
+#####################################################################
+class BiFPNBlockEDU(nn.Module):
+    """
+    !!!
+    THIS CLASS IS MEANT FOR EDUCATING MYSELF ON BIFPN.  ALL LAYERS ARE
+    HARD-CODED.
+    !!!
+
+    One BiFPN block takes feature maps at five different scales:
     (p3, p4, p5, p6, p7).
 
     Notice that the input to BiFPNBlock should have the same channel size, 
@@ -115,7 +246,7 @@ class BiFPNBlock(nn.Module):
 
     def forward(self, x):
         # Unpack the feature maps at five different scales...
-        p7, p6, p5, p4, p3 = x    # (B, C, [H], [W])
+        p3, p4, p5, p6, p7 = x    # (B, C, [H], [W])
 
         # ___/ Stage M \___
         # Fuse M6 = P7 + P6
