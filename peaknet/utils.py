6c8036f599950f5165aae150c1805b29418dbec4
carbonscott <wangimagine@gmail.com>
2023-09-13 15:21:56 -0700

    Add print_layers to utils.py.

--- peaknet/utils.py
+++ peaknet/utils.py
@@ -439,3 +439,33 @@ class Configurator(dict):
                 result[key] = value
         return result
 
+
+
+
+def print_layers(module, max_depth=1, current_indent_width=0, prints_module_name=True):
+    """
+    Recursively prints the layers of a PyTorch module.  (Keep printing child
+    element with a depth first search approach.)
+
+    Args:
+    - module (nn.Module): The current module or layer to print.
+    - current_indent_width (int): The current level of indentation for printing.
+    - prints_name (bool): Flag to determine if the name of the module should be printed.
+    """
+
+    def _print_current_layer(module, depth=0, current_indent_width=0, prints_module_name=True):
+        # Define a prefix based on current indent level
+        prefix = '  ' * current_indent_width
+
+        # Print the name and type of the current module
+        if prints_module_name: print(f"{module.__class__.__name__}", end = "")
+        print()
+
+        # Check if the current module has children
+        # If it does, recursively print each child with an increased indentation level
+        if depth < max_depth and list(module.children()):
+            for name, child in module.named_children():
+                print(f"{prefix}- ({name}): ", end = "")
+                _print_current_layer(child, depth + 1, current_indent_width + 1, prints_module_name)
+
+    _print_current_layer(module, current_indent_width=current_indent_width, prints_module_name=prints_module_name)
