8b66a0ef690db1632203176aef4086f14b870b05
carbonscott <wangimagine@gmail.com>
2023-09-20 21:56:08 -0700

    Add get_value and set_value that allows accessing value through a nested key.

--- peaknet/configurator.py
+++ peaknet/configurator.py
@@ -57,3 +57,44 @@ class Configurator(dict):
                 instance[key] = value
 
         return instance
+
+
+    def get_value(self, key):
+        """
+        Retrieve a nested key using dot-separated notation.
+        For example, for a key like "a.b.c", it fetches self['a']['b']['c'] if it exists, otherwise returns default.
+        """
+        keys  = key.split('.')
+        value = self
+        for k in keys:
+            if k in value:
+                value = value[k]
+            else:
+                raise AttributeError(
+                    f"'{type(self).__name__}' object has no attribute '{k}'. "
+                )
+
+        return value
+
+
+    def set_value(self, key, value):
+        """
+        Set a nested key using dot-separated notation.
+        For example, for a key like "a.b.c", it sets value to self['a']['b']['c'].
+        Creates necessary nested Configurators if they don't exist.
+        """
+        keys = key.split('.')
+        current_value = self
+        for i, k in enumerate(keys):
+            if i == len(keys) - 1:
+                current_value[k] = value
+            else:
+                if k not in current_value or not isinstance(current_value[k], Configurator):
+                    if self.__class__.__auto_create:
+                        current_value[k] = Configurator()
+                    else:
+                        raise AttributeError(
+                            f"'{type(self).__name__}' object has no attribute '{k}'. "
+                            "If you're trying to auto-create attributes, consider using the 'enable_auto_create' context manager in a with-statement."
+                        )
+                current_value = current_value[k]
