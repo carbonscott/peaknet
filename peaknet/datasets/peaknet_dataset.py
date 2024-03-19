830173af556be08a6de36d77f49d0fd9a26e2d16
Cong Wang <cwang31@stanford.edu>
2024-03-19 12:41:07 -0700

    Add runtime measure code.

--- peaknet/datasets/peaknet_dataset.py
+++ peaknet/datasets/peaknet_dataset.py
@@ -2,6 +2,8 @@ import csv
 import h5py
 import numpy as np
 
+from ..perf import Timer
+
 from torch.utils.data import Dataset
 
 class PeakNetDatasetLoader:
@@ -10,14 +12,13 @@ class PeakNetDatasetLoader:
     The data distribution should be handled externally (like how you construct
     the csv).
     """
-    def __init__(self, path_csv, trans_list = None, sample_size = None, applies_norm = True):
+    def __init__(self, path_csv, trans_list = None, sample_size = None, applies_norm = True, perfs_runtime = False):
         super().__init__()
 
-        self.trans_list   = trans_list
-        self.sample_size  = sample_size
-        self.applies_norm = applies_norm
-
-        self.file_cache = {}
+        self.trans_list    = trans_list
+        self.sample_size   = sample_size
+        self.applies_norm  = applies_norm
+        self.perfs_runtime = perfs_runtime
 
         # Importing data from csv...
         self.data_list = []
@@ -38,43 +39,42 @@ class PeakNetDatasetLoader:
     def get_img(self, idx):
         path_hdf5, dataset_mean, dataset_std, idx_in_hdf5 = self.data_list[idx]
 
-        if path_hdf5 not in self.file_cache:
-            self.file_cache[path_hdf5] = h5py.File(path_hdf5, 'r')
+        with Timer(tag = "Read data from hdf5", is_on = self.perfs_runtime):
+            with h5py.File(path_hdf5, 'r') as f:
+                # Obtain the image...
+                k = f"data/data_{idx_in_hdf5:04d}/image"
+                image = f.get(k)[()]
 
-        ## with h5py.File(path_hdf5, 'r') as f:
-        f = self.file_cache[path_hdf5]
+                # Obtain the label...
+                k = f"data/data_{idx_in_hdf5:04d}/label"
+                label = f.get(k)[()]
 
-        # Obtain the image...
-        k = f"data/data_{idx_in_hdf5:04d}/image"
-        image = f.get(k)[()]
+                # Obtain pixel map...
+                k = f"data/data_{idx_in_hdf5:04d}/metadata/pixel_map"
+                pixel_map_x, pixel_map_y, pixel_map_z = f.get(k)[()]
 
-        # Obtain the label...
-        k = f"data/data_{idx_in_hdf5:04d}/label"
-        label = f.get(k)[()]
+        with Timer(tag = "Assemble panels into an image", is_on = self.perfs_runtime):
+            pixel_map_x = np.round(pixel_map_x).astype(int)
+            pixel_map_y = np.round(pixel_map_y).astype(int)
+            pixel_map_z = np.round(pixel_map_z).astype(int)
 
-        # Obtain pixel map...
-        k = f"data/data_{idx_in_hdf5:04d}/metadata/pixel_map"
-        pixel_map_x, pixel_map_y, pixel_map_z = f.get(k)[()]
+            detector_image = np.zeros((pixel_map_x.max() - pixel_map_x.min() + 1,
+                                       pixel_map_y.max() - pixel_map_y.min() + 1,
+                                       pixel_map_z.max() - pixel_map_z.min() + 1), dtype = float)
+            detector_label = np.zeros((pixel_map_x.max() - pixel_map_x.min() + 1,
+                                       pixel_map_y.max() - pixel_map_y.min() + 1,
+                                       pixel_map_z.max() - pixel_map_z.min() + 1), dtype = int)
 
-        pixel_map_x = np.round(pixel_map_x).astype(int)
-        pixel_map_y = np.round(pixel_map_y).astype(int)
-        pixel_map_z = np.round(pixel_map_z).astype(int)
+            detector_image[pixel_map_x, pixel_map_y, pixel_map_z] = image
+            detector_label[pixel_map_x, pixel_map_y, pixel_map_z] = label
 
-        detector_image = np.zeros((pixel_map_x.max() - pixel_map_x.min() + 1,
-                                   pixel_map_y.max() - pixel_map_y.min() + 1,
-                                   pixel_map_z.max() - pixel_map_z.min() + 1), dtype = float)
-        detector_label = np.zeros((pixel_map_x.max() - pixel_map_x.min() + 1,
-                                   pixel_map_y.max() - pixel_map_y.min() + 1,
-                                   pixel_map_z.max() - pixel_map_z.min() + 1), dtype = int)
+        with Timer(tag = "Transpose data (some internal ops)", is_on = self.perfs_runtime):
+            detector_image = detector_image.transpose((2, 0, 1))    # (H, W, C=1) -> (C, H, W)
+            detector_label = detector_label.transpose((2, 0, 1))    # (H, W, C=1) -> (C, H, W)
 
-        detector_image[pixel_map_x, pixel_map_y, pixel_map_z] = image
-        detector_label[pixel_map_x, pixel_map_y, pixel_map_z] = label
-
-        detector_image = detector_image.transpose((2, 0, 1))    # (H, W, C=1) -> (C, H, W)
-        detector_label = detector_label.transpose((2, 0, 1))    # (H, W, C=1) -> (C, H, W)
-
-        if self.applies_norm:
-            detector_image = (detector_image - dataset_mean) / dataset_std
+        with Timer(tag = "Normalization", is_on = self.perfs_runtime):
+            if self.applies_norm:
+                detector_image = (detector_image - dataset_mean) / dataset_std
 
         return detector_image, detector_label
 
@@ -85,8 +85,9 @@ class PeakNetDatasetLoader:
         # Apply transformation to image and label at the same time...
         if self.trans_list is not None:
             data = np.concatenate([img, label], axis = 0)    # (2, H, W)
-            for trans in self.trans_list:
-                data = trans(data)
+            for enum_idx, trans in enumerate(self.trans_list):
+                with Timer(tag = f"Transform method {enum_idx:d}", is_on = self.perfs_runtime):
+                    data = trans(data)
 
             img   = data[0:1]    # (1, H, W)
             label = data[1: ]    # (1, H, W)
@@ -96,9 +97,3 @@ class PeakNetDatasetLoader:
 
     def __len__(self):
         return len(self.data_list) if self.sample_size is None else self.sample_size
-
-
-    def close_all_files(self):
-        for file in self.file_cache.values():
-            file.close()
-        self.file_cache.clear()
