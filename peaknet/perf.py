34bac51dba10f5b6b461eabebff781dcc07cd31d
Cong Wang <cwang31@stanford.edu>
2024-04-01 15:12:59 -0700

    Use a better perf.

--- peaknet/perf.py
+++ peaknet/perf.py
@@ -4,6 +4,7 @@ class Timer:
     def __init__(self, tag = None, is_on = True):
         self.tag   = tag
         self.is_on = is_on
+        self.duration = None
 
     def __enter__(self):
         if self.is_on:
@@ -14,4 +15,5 @@ class Timer:
         if self.is_on:
             self.end = time.monotonic()
             self.duration = self.end - self.start
-            print(f"{self.tag}, duration: {self.duration} sec.")
+            if self.tag is not None:
+                print(f"{self.tag}, duration: {self.duration} sec.")
