22f07ef4c1da0f6d979ad5d9a2aa0ab3ed671a3c
Cong Wang <cwang31@stanford.edu>
2024-04-02 21:42:12 -0700

    Update the ipc server example.

--- examples/ipc/server.ipc.py
+++ examples/ipc/server.ipc.py
@@ -19,25 +19,7 @@ def get_psana_img(exp, run, access_mode, detector_name):
         psana_img_buffer[key] = PsanaImg(exp, run, access_mode, detector_name)
     return psana_img_buffer[key]
 
-
-## def cleanup_shared_memory(shm):
-##     try:
-##         shm.close()
-##         shm.unlink()
-##     except FileNotFoundError:
-##         pass
-
 def worker_process(server_socket):
-    ## # Keep track of all shared memory objects created in this process
-    ## shared_memories = []
-
-    ## def cleanup_at_exit():
-    ##     for shm in shared_memories:
-    ##         cleanup_shared_memory(shm)
-
-    ## # Register the cleanup function to run when the process exits
-    ## atexit.register(cleanup_at_exit)
-
     while True:
         # Accept a new connection
         connection, client_address = server_socket.accept()
@@ -45,25 +27,23 @@ def worker_process(server_socket):
             # Receive request data
             request_data = connection.recv(4096).decode('utf-8')
             request_data = json.loads(request_data)
-            exp = request_data.get('exp')
-            run = request_data.get('run')
-            access_mode = request_data.get('access_mode')
+            exp           = request_data.get('exp')
+            run           = request_data.get('run')
+            access_mode   = request_data.get('access_mode')
             detector_name = request_data.get('detector_name')
-            event = request_data.get('event')
-            mode = request_data.get('mode')
+            event         = request_data.get('event')
+            mode          = request_data.get('mode')
 
             # Fetch psana image data
             psana_img = get_psana_img(exp, run, access_mode, detector_name)
             with Timer(tag=None, is_on=True) as t:
                 data = psana_img.get(event, None, mode)
+            print(f"Psana: {t.duration * 1e3} ms")
 
             # Keep numpy array in a shared memory
             shm = shared_memory.SharedMemory(create=True, size=data.nbytes)
             shared_array = np.ndarray(data.shape, dtype=data.dtype, buffer=shm.buf)
             shared_array[:] = data
-
-            ## # Add the shared memory object to the list for tracking
-            ## shared_memories.append(shm)
 
             response_data = json.dumps({
                 'name': shm.name,
@@ -77,15 +57,21 @@ def worker_process(server_socket):
             # Wait for the client's acknowledgment
             ack = connection.recv(1024).decode('utf-8')
             if ack == "ACK":
-                print(f"Shared memory {shm.name} ready for client to unlink.")
+                print(f"Shared memory {shm.name} ready to unlink.")
+                unlink_shared_memory(shm.name)
             else:
                 print("Did not receive proper acknowledgment from client.")
 
         finally:
-            # Cleanup if necessary
-            if shm:
-                shm.close()
             connection.close()
+
+def unlink_shared_memory(shm_name):
+    try:
+        shm = shared_memory.SharedMemory(name=shm_name)
+        shm.close()
+        shm.unlink()
+    except FileNotFoundError:
+        pass
 
 def start_server(address, num_workers):
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -101,5 +87,5 @@ def start_server(address, num_workers):
 # Example usage
 if __name__ == "__main__":
     server_address = ('localhost', 5002)  # Example address and port
-    num_workers = 10
+    num_workers = 5
     start_server(server_address, num_workers)
