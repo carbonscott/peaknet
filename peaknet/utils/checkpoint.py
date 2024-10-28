79d52139d0b2a419978df28ddcb58878e5f79e90
Cong Wang <cwang31@stanford.edu>
2024-10-28 10:38:21 -0700

    Fix rank issue and add dist.

--- peaknet/utils/checkpoint.py
+++ peaknet/utils/checkpoint.py
@@ -6,23 +6,25 @@ import os
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import torch.distributed as dist
 
 import logging
 
 logger = logging.getLogger(__name__)
 
 class Checkpoint:
-    MODEL_STATE_DICT_FILE = 'model_full_state_dict.pt'
-    OPTIM_STATE_DICT_FILE = 'optim_full_state_dict.pt'
-    LR_STATE_DICT_FILE    = 'lr_full_state_dict.pt'
-    ITER_STATE_DICT_FILE  = 'iter_full_state_dict.pt'
+    MODEL_STATE_DICT_FILE = 'model_state_dict.pt'
+    OPTIM_STATE_DICT_FILE = 'optim_state_dict.pt'
+    LR_STATE_DICT_FILE    = 'lr_state_dict.pt'
+    ITER_STATE_DICT_FILE  = 'iter_state_dict.pt'
 
     def __init__(self):
         pass
 
     def save_model_checkpoint(self, rank, model, path_checkpoint_model):
-        model_state_dict = model.state_dict()
-        torch.save(model_state_dict, path_checkpoint_model)
+        if rank == 0:
+            model_state_dict = model.state_dict()
+            torch.save(model_state_dict, path_checkpoint_model)
 
     def load_model_checkpoint(self, rank, model, path_checkpoint_model):
         """
@@ -36,23 +38,26 @@ class Checkpoint:
         optimizer.load_state_dict(full_optim_state_dict)
 
     def save_optimizer_checkpoint(self, rank, model, optimizer, path_checkpoint_optim):
-        optim_state_dict = optimizer.state_dict()
-        torch.save(optim_state_dict, path_checkpoint_optim)
+        if rank == 0:
+            optim_state_dict = optimizer.state_dict()
+            torch.save(optim_state_dict, path_checkpoint_optim)
 
     def load_optimizer_checkpoint(self, rank, model, optimizer, path_checkpoint_optim):
         optim_state_dict = torch.load(path_checkpoint_optim)
         optimizer.load_state_dict(optim_state_dict)
 
     def save_lr_checkpoint(self, rank, lr_scheduler, path_checkpoint_lr):
-        lr_state_dict = lr_scheduler.state_dict()
-        torch.save(lr_state_dict, path_checkpoint_lr)
+        if rank == 0:
+            lr_state_dict = lr_scheduler.state_dict()
+            torch.save(lr_state_dict, path_checkpoint_lr)
 
     def load_lr_checkpoint(self, rank, lr_scheduler, path_checkpoint_lr):
         lr_state_dict = torch.load(path_checkpoint_lr, map_location = 'cpu')
         lr_scheduler.load_state_dict(lr_state_dict)
 
     def save_iter_state_checkpoint(self, rank, iter_state, path_checkpoint_iter_state):
-        torch.save(iter_state, path_checkpoint_iter_state)
+        if rank == 0:
+            torch.save(iter_state, path_checkpoint_iter_state)
 
     def load_iter_state_checkpoint(self, rank, iter_state, path_checkpoint_iter_state):
         iter_state_saved = torch.load(path_checkpoint_iter_state, map_location = 'cpu')
